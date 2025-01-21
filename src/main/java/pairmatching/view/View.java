e34c23cedf6f5ca43ff109819015fa608b11d4ce
jaehyeonjung0613 <jaehyeonjung0613@gmail.com>
2025-01-21 15:12:37 +0900

    feat: 13-1. 화면 기본 인터페이스 정의 - [View.java] 화면 기본 인터페이스 정의

---
+++ src/main/java/pairmatching/view/View.java
@@ -0,0 +1,8 @@
+package pairmatching.view;
+
+import pairmatching.ui.InputHelper;
+import pairmatching.ui.OutputHelper;
+
+public interface View {
+    void execute(InputHelper inputHelper, OutputHelper outputHelper);
+}
